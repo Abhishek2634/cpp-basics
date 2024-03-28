006a8b9f24d46bf9b23fba1aa31c94dae416f1cc
abhishek farshwal <abhishekfarshwal2634@gmail.com>
2024-03-28 21:35:00 +0530

    new file added

---
+++ input.cpp
@@ -0,0 +1,35 @@
+# include <iostream>
+using namespace std;
+int main(){
+    // taking int as input 
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << a << endl;
+    cout << a << endl;
+    cout << a << endl;
+
+    // string  input 
+    string m;
+    cin >> m;
+    cout << m << endl;
+
+    // string line input
+    string k ;
+    getline(cin,k);
+    cout << k << endl;
+
+
+    // conditional statements
+    bool isTrue;
+    int age ;
+    cin >> age >> isTrue;
+    if(age > 30){
+        cout << "well done" << endl;
+    }else if (isTrue){
+        cout << "okay" << endl;
+    }else {
+        cout << "nothing" << endl;
+    }
+
+
+}

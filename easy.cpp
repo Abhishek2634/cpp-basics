48528db74a30fa1f587ca0123432b30f4cc7524f
abhishek farshwal <abhishekfarshwal2634@gmail.com>
2024-04-11 18:05:25 +0530

    added new file

---
+++ easy.cpp
@@ -0,0 +1,12 @@
+# include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a ;
+    int b ;
+    cin >> a >> b;
+    double m =  (double) a/b;
+    cout << "floor " << a << " / " << b << " = " << floor(m) << '\n' ;
+    cout << "ceil " << a << " / " << b << " = " << ceil(m)   << '\n';
+    cout << "round " << a << " / " << b <<  " = " <<round(m)  <<'\n' ;
+
+}

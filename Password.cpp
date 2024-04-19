eedf93f39264e9adb315ea9872594a5ed1f2d4a5
abhishek farshwal <abhishekfarshwal2634@gmail.com>
2024-04-19 19:13:45 +0530

    added file

---
+++ Password.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST std::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
+int main(){
+    FAST;
+    while(true){
+        int a;
+        cin >> a;
+        if(a == 1999){
+            cout << "Correct"<<'\n';
+            return 0;
+        }else{
+            cout << "Wrong"<<'\n';
+        }
+    }
+}

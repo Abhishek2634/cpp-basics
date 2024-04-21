dbee9475abc2aa90e8796f10a6db0f8459913b69
abhishek farshwal <abhishekfarshwal2634@gmail.com>
2024-04-21 11:20:04 +0530

    added file

---
+++ Table.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin >> a;
+    int n = 1;
+    while(n <= 12){
+        cout << a << " * " << n << " = "<< a*n << endl;
+        n++;
+    }  
+}

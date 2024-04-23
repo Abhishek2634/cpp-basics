e1c10db1212ef31c871596cc43393332909791f2
abhishek farshwal <abhishekfarshwal2634@gmail.com>
2024-04-23 23:41:04 +0530

    added file

--- Pattern.cpp
+++ Pattern.cpp
@@ -1,6 +1,8 @@
 # include <bits/stdc++.h>
 using namespace std;
+void another();
 int main(){
+    int m;
     for(int i = 0; i < 5;i++){
         for(int j = 0; j < 5-i;j ++){
             cout << "*";
@@ -10,5 +12,20 @@ int main(){
         }
         cout << endl;
 
+    }
+    another();
+}
+void another(){
+    for(int i = 1; i <= 4;i++){
+        for(int j = 4;j > i;j--){
+            cout << " ";
+        }
+        for(int j = 1; j < i;j++){
+            cout << j ;
+        }
+      for(int j = i ; j >= 1;j--){
+        cout << j;
+      }
+      cout << endl;
     }
 }

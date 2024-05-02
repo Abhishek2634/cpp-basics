9452f796abdc93319fd11411d9946cefcc91420f
abhishek farshwal <abhishekfarshwal2634@gmail.com>
2024-05-02 10:13:59 +0530

    added file

---
+++ OddSum.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int t ;
+    cin >> t;
+    while(t-- > 0){
+        int a ,b;
+        cin >> a >> b;
+        int sum = 0;
+        if(a > b){
+            for(int i = b+1;i < a;i++){
+                if(i % 2 != 0)
+                sum += i;
+            }
+        }else{
+            for(int i = a+1;i < b;i++){
+                if(i % 2 != 0)
+                sum += i;
+            }
+        }
+        cout << sum << endl;
+    }
+}

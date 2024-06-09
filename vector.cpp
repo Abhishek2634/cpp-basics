c8f6aacb98808bfad7689bf9e8785a5abd4d7cb1
abhishek farshwal <abhishekfarshwal2634@gmail.com>
2024-06-09 18:48:05 +0530

    added new file

---
+++ vector.cpp
@@ -0,0 +1,20 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // vector <int> v;
+    // v.push_back(1);
+    // v.push_back(2);
+    // v.push_back(3);
+    // cout << v.size();
+    // cout << v[0] <<'\n';
+    //  cout << v[1] <<'\n';
+    // cout << v[2] <<'\n';
+    // v.pop_back();
+    // cout << v.size();
+
+    vector<int>v (20, 9);
+    cout << v[10];
+
+    return 0;
+}
